38149ae54fae542c3467eefe75db275c40cb6f0a
Denis Berekelya <cm-den@yandex.ru>
2023-05-06 23:31:37 +0300

    Выполнена нарезка на компоненты

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import {render, RenderPosition, } from './render.js';
+import TripInfoView from './view/trip-info.js';
+import TripSortView from './view/trip-sort.js';
+import EventEditView from './view/event-edit.js';
+import EventAddView from './view/event-add.js';
+import EventView from './view/event.js';
+import FiltersView from './view/trip-filters.js';
+
+const filterContainer = document.querySelector('.trip-controls__filters');
+const tripEventsContainer = document.querySelector('.trip-events');
+const tripMainContainer = document.querySelector('.trip-main');
+
+render(new TripInfoView(), tripMainContainer, RenderPosition.AFTERBEGIN);
+render(new FiltersView(), filterContainer);
+render(new TripSortView(), tripEventsContainer);
+render(new EventAddView(), tripEventsContainer);
+render(new EventView(), tripEventsContainer);
+render(new EventView(), tripEventsContainer);
+render(new EventView(), tripEventsContainer);
+

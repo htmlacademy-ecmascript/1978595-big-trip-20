273571ebd89073041b6825eb1abdef2f3d6e2691
Denis Berekelya <cm-den@yandex.ru>
2023-05-09 18:28:16 +0300

    Presenter

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,10 +1,10 @@
-import {render, RenderPosition, } from './render.js';
-import TripInfoView from './view/trip-info.js';
-import TripSortView from './view/trip-sort.js';
-import EventEditView from './view/event-edit.js';
-import EventAddView from './view/event-add.js';
-import EventView from './view/event.js';
-import FiltersView from './view/trip-filters.js';
+// import {render, RenderPosition, } from './render.js';
+// import TripInfoView from './view/trip-info.js';
+// import TripSortView from './view/trip-sort.js';
+// import EventEditView from './view/event-edit.js';
+// import EventAddView from './view/event-add.js';
+// import EventView from './view/event.js';
+// import FiltersView from './view/trip-filters.js';
 
 export default class Presenter {
 
